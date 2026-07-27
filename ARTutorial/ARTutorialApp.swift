e20ee802d5e572c273014d3541a96082910cd16d
reynardamadeus <reynard.starlee@gmail.com>
2026-07-27 21:21:23 +0700

    Initial Commit

---
+++ ARTutorial/ARTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARTutorialApp.swift
+//  ARTutorial
+//
+//  Created by Reynard Amadeus  on 27/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ARTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARTutorial
+//
+//  Created by Reynard Amadeus  on 27/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
